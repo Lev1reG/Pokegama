b8713cc7ec575f7eededb6b5b902f1d46864743a
Lev1reG <derentanaphan@mail.ugm.ac.id>
2024-11-28 19:23:42 +0700

    feat: add maps on facility page

--- app/src/main/java/com/example/pokegama/ui/facilities/FacilitiesFragment.kt
+++ app/src/main/java/com/example/pokegama/ui/facilities/FacilitiesFragment.kt
@@ -1,5 +1,6 @@
 package com.example.pokegama.ui.facilities
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -12,6 +13,10 @@ import com.example.pokegama.databinding.FragmentFacilitiesBinding
 import com.example.pokegama.ui.adapter.FacilitiesAdapter
 import com.example.pokegama.ui.dialogs.NoInternetDialogFragment
 import com.example.pokegama.util.*
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraBoundsOptions
+import com.mapbox.maps.CoordinateBounds
+import com.mapbox.maps.Style
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -21,6 +26,42 @@ class FacilitiesFragment : Fragment(R.layout.fragment_facilities) {
     private val binding by viewBinding(FragmentFacilitiesBinding::bind)
     private val viewModel by viewModels<FacilitiesViewModel>()
     private val facilityAdapter = FacilitiesAdapter()
+    private val internetChecker: InternetChecker by lazy { InternetChecker(requireContext()) }
+
+    private fun handleMapView() {
+        val ugmBounds = CoordinateBounds(
+            Point.fromLngLat(110.36288772392231, -7.785549599115427), // Southwest corner
+            Point.fromLngLat(110.39603271480053, -7.758165106236248)  // Northeast corner
+        )
+
+        val isOnline = internetChecker.hasInternetConnection()
+
+        if (isOnline) {
+            binding.mapView.mapboxMap.loadStyle(Style.MAPBOX_STREETS) { style ->
+                val bitmap = BitmapFactory.decodeResource(resources, R.drawable.red_pin)
+                style.addImage("red-pin-icon-id", bitmap)
+            }
+        } else {
+            binding.mapView.mapboxMap.loadStyle(Style.LIGHT) { style ->
+                val bitmap = BitmapFactory.decodeResource(resources, R.drawable.red_pin)
+                style.addImage("red-pin-icon-id", bitmap)
+            }
+        }
+
+        binding.mapView.mapboxMap.setBounds(
+            CameraBoundsOptions.Builder()
+                .bounds(ugmBounds)
+                .minZoom(14.0)
+                .build()
+        )
+
+        binding.mapView.mapboxMap.setCamera(
+            com.mapbox.maps.CameraOptions.Builder()
+                .center(Point.fromLngLat(110.37762, -7.77083))
+                .zoom(14.0)
+                .build()
+        )
+    }
 
     private fun collectFacilityType() {
         arguments?.let {
@@ -66,6 +107,7 @@ class FacilitiesFragment : Fragment(R.layout.fragment_facilities) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        handleMapView()
         collectFacilityType()
         initRecyclerView()
         collectUiState()
